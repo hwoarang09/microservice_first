6d0ad43238b068c76aa53f22e6818a9680b824d8
ysungwon <hwoarang09@naver.com>
2024-03-16 04:06:53 +0900

    hi

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+//이번에는 DB대신 메모리에 저장
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  //4byte 랜덤아이디
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
