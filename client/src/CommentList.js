1a5837b1da4b482adcad6de0c0b3cd2cfccd6820
ysungwon <hwoarang09@naver.com>
2024-03-23 22:59:12 +0900

    before docker

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async (postId) => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  //딱한번만 처음에 실행
-  useEffect(() => {
-    fetchComments(postId);
-  }, [postId]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = Object.values(comments).map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "This comment is awaing moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "This comment has beenn rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
   return <ul>{renderedComments}</ul>;
 };
